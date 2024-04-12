speedy005/oscam

commit 8e8dc4c8a2cd1ac7a71ca6a7b46a939acd7cce63

Generate svn like revision number and additionally display git commit sha

config.sh
@@ -98,6 +98,7 @@ Usage: `basename $0` [parameters]
 
  -v, --oscam-version       Display OSCam version.
  -r, --oscam-revision      Display OSCam SVN revision.
+ -c, --oscam-commit        Display OSCam GIT short commit sha 8-digits.
 
  -O, --detect-osx-sdk-version  Find where OS X SDK is located
 
@@ -703,12 +704,14 @@ do
 		break
 	;;
 	'-r'|'--oscam-revision')
-		revision=`(svnversion -n . 2>/dev/null || printf 0) | sed 's/.*://; s/[^0-9]*$//; s/^$/0/'`
-		if [ "$revision" = "0" ]
-		then
-			which git > /dev/null 2>&1 && revision=`git log -10 --pretty=%B | grep git-svn-id | head -n 1 | sed -n -e 's/^.*trunk@\([0-9]*\) .*$/\1/p'`
-		fi
-		echo $revision
+		offset=1103 #difference between last svn revision number and git commit count (without merge commits) + 1
+		revision=`git rev-list --no-merges --count HEAD`
+		echo $(($offset + $revision))
+		break
+	;;
+	'-c'|'--oscam-commit') 
+		sha=`git log 2>/dev/null | sed -n 1p | cut -d ' ' -f2 | cut -c1-8`
+		echo $sha
 		break
 	;;
 	'-O'|'--detect-osx-sdk-version')

module-webif-tpl.c
@@ -588,7 +588,7 @@ int32_t tpl_saveIncludedTpls(const char *path)
 		{
 			if(strncmp(tpl->tpl_name, "IC", 2) != 0)
 			{
-				fprintf(fp, "<!--OSCam;%d;%s;%s;%s-->\n", crc32(0, (uint8_t *)tpl->tpl_data, tpl->tpl_data_len), CS_VERSION, CS_SVN_VERSION, tpl->tpl_deps);
+				fprintf(fp, "<!--OSCam;%d;%s;%s@%s;%s-->\n", crc32(0, (uint8_t *)tpl->tpl_data, tpl->tpl_data_len), CS_VERSION, CS_SVN_VERSION, CS_GIT_COMMIT, tpl->tpl_deps);
 			}
 			fwrite(tpl->tpl_data, tpl->tpl_data_len, 1, fp);
 			fclose(fp);
@@ -638,7 +638,7 @@ void tpl_checkOneDirDiskRevisions(const char *subdir)
 				else { error = 0; }
 			}
 			else { cs_log("WARNING: Your http disk template %s is in the old template format without revision info. Please consider upgrading it!", path); }
-			if(error) { cs_log("If you are sure that it is current, add the following line at the beginning of the template to suppress this warning: <!--OSCam;%lu;%s;%s;%s-->", curchecksum, CS_VERSION, CS_SVN_VERSION, ifdefs); }
+			if(error) { cs_log("If you are sure that it is current, add the following line at the beginning of the template to suppress this warning: <!--OSCam;%lu;%s;%s@%s;%s-->", curchecksum, CS_VERSION, CS_SVN_VERSION, CS_GIT_COMMIT, ifdefs); }
 			NULLFREE(tplorg);
 		}
 	}

CMakeLists.txt
@@ -683,6 +683,8 @@ add_custom_target (
 
 execute_process (COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/config.sh --oscam-revision COMMAND tr -d '\n' OUTPUT_VARIABLE CS_SVN_VERSION)
 add_definitions ("-D'CS_SVN_VERSION=\"${CS_SVN_VERSION}\"'")
+execute_process (COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/config.sh --oscam-commit COMMAND tr -d '\n' OUTPUT_VARIABLE CS_GIT_COMMIT)
+add_definitions ("-D'CS_GIT_COMMIT=\"${CS_GIT_COMMIT}\"'")
 
 execute_process(COMMAND ${CMAKE_C_COMPILER} -dumpmachine COMMAND tr -d '\n' OUTPUT_VARIABLE CS_TARGET)
 add_definitions ("-D'CS_TARGET=\"${CS_TARGET}\"'")
@@ -850,6 +852,7 @@ message (STATUS "")
 message (STATUS "  operating system: ${OSCamOperatingSystem}")
 message (STATUS "  target system: ${CS_TARGET}")
 message (STATUS "  revision: ${CS_SVN_VERSION}")
+message (STATUS "  commit: ${CS_GIT_COMMIT}")
 if (HAVE_LIBCRYPTO)
 	message (STATUS "  use system libcrypto functions")
 else (HAVE_LIBCRYPTO)

Makefile
@@ -6,6 +6,7 @@ SHELL = /bin/sh
 
 VER     := $(shell ./config.sh --oscam-version)
 SVN_REV := $(shell ./config.sh --oscam-revision)
+GIT_SHA := $(shell ./config.sh --oscam-commit)
 
 uname_S := $(shell sh -c 'uname -s 2>/dev/null || echo not')
 
@@ -61,6 +62,7 @@ endif
 
 override STD_LIBS := -lm $(LIB_PTHREAD) $(LIB_DL) $(LIB_RT)
 override STD_DEFS := -D'CS_SVN_VERSION="$(SVN_REV)"'
+override STD_DEFS += -D'CS_GIT_COMMIT="$(GIT_SHA)"'
 override STD_DEFS += -D'CS_CONFDIR="$(CONF_DIR)"'
 
 CC = $(CROSS_DIR)$(CROSS)gcc
@@ -233,9 +235,9 @@ OBJDIR := $(BUILD_DIR)/$(TARGET)
 # These variables will be used to select only needed files for compilation
 -include $(OBJDIR)/config.mak
 
-OSCAM_BIN := $(BINDIR)/oscam-$(VER)$(SVN_REV)-$(subst cygwin,cygwin.exe,$(TARGET))
+OSCAM_BIN := $(BINDIR)/oscam-$(VER)r$(SVN_REV)@$(GIT_SHA)-$(subst cygwin,cygwin.exe,$(TARGET))
 TESTS_BIN := tests.bin
-LIST_SMARGO_BIN := $(BINDIR)/list_smargo-$(VER)$(SVN_REV)-$(subst cygwin,cygwin.exe,$(TARGET))
+LIST_SMARGO_BIN := $(BINDIR)/list_smargo-$(VER)r$(SVN_REV)@$(GIT_SHA)-$(subst cygwin,cygwin.exe,$(TARGET))
 
 # Build list_smargo-.... only when WITH_LIBUSB build is requested.
 ifndef USE_LIBUSB
@@ -401,7 +403,7 @@ all:
 	@-mkdir -p $(OBJDIR)/cscrypt $(OBJDIR)/csctapi $(OBJDIR)/minilzo $(OBJDIR)/webif
 	@-printf "\
 +-------------------------------------------------------------------------------\n\
-| OSCam ver: $(VER) rev: $(SVN_REV) target: $(TARGET)\n\
+| OSCam ver: $(VER) rev: $(SVN_REV) sha: $(GIT_SHA) target: $(TARGET)\n\
 | Tools:\n\
 |  CROSS    = $(CROSS_DIR)$(CROSS)\n\
 |  CC       = $(CC)\n\
@@ -711,8 +713,8 @@ OSCam build system documentation\n\
 \n\
    OSCAM_BIN=text  - This variable controls how the oscam binary will be named.\n\
                      Default OSCAM_BIN value is:\n\
-                      'BINDIR/oscam-VERSVN_REV-TARGET'\n\
-                     Once the variables (BINDIR, VER, SVN_REV and TARGET) are\n\
+                      'BINDIR/oscam-VERSVN_REV@$GIT_SHA-TARGET'\n\
+                     Once the variables (BINDIR, VER, SVN_REV, GIT_SHA and TARGET) are\n\
                      replaced, the resulting filename can look like this:\n\
                       'Distribution/oscam-1.20-unstable_svn7404-i486-slackware-linux-static'\n\
                      For example you can run: 'make OSCAM_BIN=my-oscam'\n\

README
@@ -32,21 +32,21 @@ Version history
 ===============
 
 OSCam history is accessible through Trac timeline at:
-   https://trac.streamboard.tv/oscam/timeline
+   https://git.streamboard.tv/common/oscam/-/commits/master
 
 
 Repositories
 ============
 
-SVN repository:
-   svn checkout https://svn.streamboard.tv/oscam/trunk oscam-svn
+GIT repository:
+   git clone https://git.streamboard.tv/common/oscam.git oscam
 
 
 Building OSCam from source
 ==========================
 
  - Get the lastest sources from SVN
-    svn checkout https://svn.streamboard.tv/oscam/trunk oscam-trunk
+    git clone https://git.streamboard.tv/common/oscam.git oscam
 
  - Go to oscam-trunk directory.
 
@@ -64,7 +64,7 @@ Building OScam for different CPUs (cross-compilation)
 First you need to install the target CPU toolchain. Already built toolchains
 for various architectures can be downloaded from:
 
-    https://trac.streamboard.tv/oscam/wiki/crosscompiling
+    https://git.streamboard.tv/common/oscam/-/wikis/CrossCompiling
 
 In order to cross compile OSCam you need to set CROSS variable when
 running make. For example to compile for SH4 architecture you need
@@ -105,7 +105,7 @@ Help and Support
 man pages and configuration examples are in Distribution/doc directory.
 
 You may visit our Trac system for tracking and filling bug reports.
-   https://trac.streamboard.tv/oscam/newticket
+   https://git.streamboard.tv/common/oscam/-/issues/new
 
 If you experience any problems with OSCam, feel free to post in our support
 forum under (mainly German and English language) at:

globals.h
@@ -368,9 +368,12 @@
 /* ===========================
  *			constants
  * =========================== */
-#define CS_VERSION				"1.30_svn"
+#define CS_VERSION				"1.30"
 #ifndef CS_SVN_VERSION
 # define CS_SVN_VERSION			"test"
+#endif
+#ifndef CS_GIT_COMMIT
+# define CS_GIT_COMMIT			"a2b4c8d8"
 #endif
 #ifdef CS_CACHEEX
 #ifdef CS_CACHEEX_AIO

module-cccam.c
@@ -2820,11 +2820,11 @@ int32_t cc_parse_msg(struct s_client *cl, uint8_t *buf, int32_t l)
 				{
 					uint8_t token[256];
 #ifdef CS_CACHEEX_AIO
-					snprintf((char *)token, sizeof(token), "PARTNER: OSCam v%s, build r%s (%s) [EXT,SID,SLP,LGF]",
+					snprintf((char *)token, sizeof(token), "PARTNER: OSCam v%s, build r%s@%s (%s) [EXT,SID,SLP,LGF]",
 #else
-					snprintf((char *)token, sizeof(token), "PARTNER: OSCam v%s, build r%s (%s) [EXT,SID,SLP]",
+					snprintf((char *)token, sizeof(token), "PARTNER: OSCam v%s, build r%s@%s (%s) [EXT,SID,SLP]",
 #endif
-								CS_VERSION, CS_SVN_VERSION, CS_TARGET);
+								CS_VERSION, CS_SVN_VERSION, CS_GIT_COMMIT, CS_TARGET);
 
 					cc_cmd_send(cl, token, cs_strlen((char *)token) + 1, MSG_CW_NOK1);
 				}
@@ -3164,8 +3164,8 @@ int32_t cc_parse_msg(struct s_client *cl, uint8_t *buf, int32_t l)
 						}
 
 						uint8_t token[256];
-						snprintf((char *)token, sizeof(token), "PARTNER: OSCam v%s, build r%s (%s)%s",
-								CS_VERSION, CS_SVN_VERSION, CS_TARGET, param);
+						snprintf((char *)token, sizeof(token), "PARTNER: OSCam v%s, build r%s@%s (%s)%s",
+								CS_VERSION, CS_SVN_VERSION, CS_GIT_COMMIT, CS_TARGET, param);
 
 						cc_cmd_send(cl, token, cs_strlen((char *)token) + 1, MSG_CW_NOK1);
 					}

module-dvbapi.c
@@ -673,8 +673,8 @@ int32_t dvbapi_net_send(uint32_t request, int32_t socket_fd, uint32_t msgid, int
 				cs_strncat(capabilities, ",e2", sizeof(capabilities)); // usage of DES algo signalled through PID index - CSA and DES only
 			}
 
-			*info_len = snprintf((char *) &packet[size], sizeof(packet) - size, "OSCam v%s, build r%s (%s); %s",
-						CS_VERSION, CS_SVN_VERSION, CS_TARGET, capabilities + 1);
+			*info_len = snprintf((char *) &packet[size], sizeof(packet) - size, "OSCam v%s, build r%s@%s (%s); %s",
+						CS_VERSION, CS_SVN_VERSION, CS_GIT_COMMIT, CS_TARGET, capabilities + 1);
 
 			size += *info_len;
 			break;

commit 344ea3ee1b3f3983aba0d4e336343c1f4472ce28

Fix webif utf-8 encoding - fix wrong display of utf-8 characters on webif readers page (introduced in e64a1563) - Re-enable Base64 object in order to fix UTF8 characters on webif live log page (removed in 469a6022) - fix whitespace typo  (introduced in 8e8dc4c8) - Closes #4928
Co-authored-by: @ohmza

config.sh
@@ -709,7 +709,7 @@ do
 		echo $(($offset + $revision))
 		break
 	;;
-	'-c'|'--oscam-commit') 
+	'-c'|'--oscam-commit')
 		sha=`git log 2>/dev/null | sed -n 1p | cut -d ' ' -f2 | cut -c1-8`
 		echo $sha
 		break

module-webif-tpl.c
@@ -785,7 +785,7 @@ char *xml_encode(struct templatevars *vars, const char *chartoencode)
 			pos += 1;
 			break;
 		default:
-			if(tmp < 32 || tmp > 127)
+			if(tmp < 32)
 			{
 				snprintf(buffer, 7, "&#%d;", tmp);
 				memcpy(encoded + pos, buffer, cs_strlen(buffer));

webif/include/jscript.js
@@ -954,10 +954,10 @@ function updateLogpage(data) {
 	}
 
 	$.each(data.oscam.lines, function (i, item) {
-		var entry = atob(item.line);
-		if (isWhitelisted(entry)) {
-			var newcolor = getLogColor(entry);
-			var newline = $('<li class="' + decodeURIComponent(item.usr) + '">' + entry + '</li>\n');
+
+		if (isWhitelisted(Base64.decode(item.line))) {
+			var newcolor = getLogColor(Base64.decode(item.line));
+			var newline = $('<li class="' + decodeURIComponent(item.usr) + '">' + Base64.decode(item.line) + '</li>\n');
 			var hiddenline = 0;
 			if (newcolor) {
 				if (newcolor.hidden != '1') {
@@ -2528,3 +2528,6 @@ function decodeVideoguardEMM(text, target, addHideButton) {
 		}
 	}
 })(jQuery)
+
+// Create Base64 Object
+var Base64={_keyStr:"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",encode:function(e){var t="";var n,r,i,s,o,u,a;var f=0;e=Base64._utf8_encode(e);while(f<e.length){n=e.charCodeAt(f++);r=e.charCodeAt(f++);i=e.charCodeAt(f++);s=n>>2;o=(n&3)<<4|r>>4;u=(r&15)<<2|i>>6;a=i&63;if(isNaN(r)){u=a=64}else if(isNaN(i)){a=64}t=t+this._keyStr.charAt(s)+this._keyStr.charAt(o)+this._keyStr.charAt(u)+this._keyStr.charAt(a)}return t},decode:function(e){var t="";var n,r,i;var s,o,u,a;var f=0;e=e.replace(/[^A-Za-z0-9\+\/\=]/g,"");while(f<e.length){s=this._keyStr.indexOf(e.charAt(f++));o=this._keyStr.indexOf(e.charAt(f++));u=this._keyStr.indexOf(e.charAt(f++));a=this._keyStr.indexOf(e.charAt(f++));n=s<<2|o>>4;r=(o&15)<<4|u>>2;i=(u&3)<<6|a;t=t+String.fromCharCode(n);if(u!=64){t=t+String.fromCharCode(r)}if(a!=64){t=t+String.fromCharCode(i)}}t=Base64._utf8_decode(t);return t},_utf8_encode:function(e){e=e.replace(/\r\n/g,"\n");var t="";for(var n=0;n<e.length;n++){var r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r)}else if(r>127&&r<2048){t+=String.fromCharCode(r>>6|192);t+=String.fromCharCode(r&63|128)}else{t+=String.fromCharCode(r>>12|224);t+=String.fromCharCode(r>>6&63|128);t+=String.fromCharCode(r&63|128)}}return t},_utf8_decode:function(e){var t="";var n=0;var r=c1=c2=0;while(n<e.length){r=e.charCodeAt(n);if(r<128){t+=String.fromCharCode(r);n++}else if(r>191&&r<224){c2=e.charCodeAt(n+1);t+=String.fromCharCode((r&31)<<6|c2&63);n+=2}else{c2=e.charCodeAt(n+1);c3=e.charCodeAt(n+2);t+=String.fromCharCode((r&15)<<12|(c2&63)<<6|c3&63);n+=3}}return t}}