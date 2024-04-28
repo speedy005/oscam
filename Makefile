speedy005/oscam

commit 8e8dc4c8a2cd1ac7a71ca6a7b46a939acd7cce63

Generate svn like revision number and additionally display git commit sha

CMakeLists.txt
@@ -683,6 +683,8 @@ add_custom_target (
 
 execute_process (COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/config.sh --oscam-revision COMMAND tr -d '\n' OUTPUT_VARIABLE CS_SVN_VERSION)
 add_definitions ("-D'CS_SVN_VERSION=\"${CS_SVN_VERSION}\"'")
+execute_process (COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/config.sh --oscam-commit COMMAND tr -d '\n' OUTPUT_VARIABLE CS_GIT_COMMIT)
+add_definitions ("-D'CS_GIT_COMMIT=\"${CS_GIT_COMMIT}\"'")
 
 execute_process(COMMAND ${CMAKE_C_COMPILER} -dumpmachine COMMAND tr -d '\n' OUTPUT_VARIABLE CS_TARGET)
 add_definitions ("-D'CS_TARGET=\"${CS_TARGET}\"'")
@@ -850,6 +852,7 @@ message (STATUS "")
 message (STATUS "  operating system: ${OSCamOperatingSystem}")
 message (STATUS "  target system: ${CS_TARGET}")
 message (STATUS "  revision: ${CS_SVN_VERSION}")
+message (STATUS "  commit: ${CS_GIT_COMMIT}")
 if (HAVE_LIBCRYPTO)
 	message (STATUS "  use system libcrypto functions")
 else (HAVE_LIBCRYPTO)

Makefile
@@ -6,6 +6,7 @@ SHELL = /bin/sh
 
 VER     := $(shell ./config.sh --oscam-version)
 SVN_REV := $(shell ./config.sh --oscam-revision)
+GIT_SHA := $(shell ./config.sh --oscam-commit)
 
 uname_S := $(shell sh -c 'uname -s 2>/dev/null || echo not')
 
@@ -61,6 +62,7 @@ endif
 
 override STD_LIBS := -lm $(LIB_PTHREAD) $(LIB_DL) $(LIB_RT)
 override STD_DEFS := -D'CS_SVN_VERSION="$(SVN_REV)"'
+override STD_DEFS += -D'CS_GIT_COMMIT="$(GIT_SHA)"'
 override STD_DEFS += -D'CS_CONFDIR="$(CONF_DIR)"'
 
 CC = $(CROSS_DIR)$(CROSS)gcc
@@ -233,9 +235,9 @@ OBJDIR := $(BUILD_DIR)/$(TARGET)
 # These variables will be used to select only needed files for compilation
 -include $(OBJDIR)/config.mak
 
-OSCAM_BIN := $(BINDIR)/oscam-$(VER)$(SVN_REV)-$(subst cygwin,cygwin.exe,$(TARGET))
+OSCAM_BIN := $(BINDIR)/oscam-$(VER)r$(SVN_REV)@$(GIT_SHA)-$(subst cygwin,cygwin.exe,$(TARGET))
 TESTS_BIN := tests.bin
-LIST_SMARGO_BIN := $(BINDIR)/list_smargo-$(VER)$(SVN_REV)-$(subst cygwin,cygwin.exe,$(TARGET))
+LIST_SMARGO_BIN := $(BINDIR)/list_smargo-$(VER)r$(SVN_REV)@$(GIT_SHA)-$(subst cygwin,cygwin.exe,$(TARGET))
 
 # Build list_smargo-.... only when WITH_LIBUSB build is requested.
 ifndef USE_LIBUSB
@@ -401,7 +403,7 @@ all:
 	@-mkdir -p $(OBJDIR)/cscrypt $(OBJDIR)/csctapi $(OBJDIR)/minilzo $(OBJDIR)/webif
 	@-printf "\
 +-------------------------------------------------------------------------------\n\
-| OSCam ver: $(VER) rev: $(SVN_REV) target: $(TARGET)\n\
+| OSCam ver: $(VER) rev: $(SVN_REV) sha: $(GIT_SHA) target: $(TARGET)\n\
 | Tools:\n\
 |  CROSS    = $(CROSS_DIR)$(CROSS)\n\
 |  CC       = $(CC)\n\
@@ -711,8 +713,8 @@ OSCam build system documentation\n\
 \n\
    OSCAM_BIN=text  - This variable controls how the oscam binary will be named.\n\
                      Default OSCAM_BIN value is:\n\
-                      'BINDIR/oscam-VERSVN_REV-TARGET'\n\
-                     Once the variables (BINDIR, VER, SVN_REV and TARGET) are\n\
+                      'BINDIR/oscam-VERSVN_REV@$GIT_SHA-TARGET'\n\
+                     Once the variables (BINDIR, VER, SVN_REV, GIT_SHA and TARGET) are\n\
                      replaced, the resulting filename can look like this:\n\
                       'Distribution/oscam-1.20-unstable_svn7404-i486-slackware-linux-static'\n\
                      For example you can run: 'make OSCAM_BIN=my-oscam'\n\

config.sh
@@ -98,6 +98,7 @@ Usage: `basename $0` [parameters]
 
  -v, --oscam-version       Display OSCam version.
  -r, --oscam-revision      Display OSCam SVN revision.
+ -c, --oscam-commit        Display OSCam GIT short commit sha 8-digits.
 
  -O, --detect-osx-sdk-version  Find where OS X SDK is located
 
@@ -703,12 +704,14 @@ do
 		break
 	;;
 	'-r'|'--oscam-revision')
-		revision=`(svnversion -n . 2>/dev/null || printf 0) | sed 's/.*://; s/[^0-9]*$//; s/^$/0/'`
-		if [ "$revision" = "0" ]
-		then
-			which git > /dev/null 2>&1 && revision=`git log -10 --pretty=%B | grep git-svn-id | head -n 1 | sed -n -e 's/^.*trunk@\([0-9]*\) .*$/\1/p'`
-		fi
-		echo $revision
+		offset=1103 #difference between last svn revision number and git commit count (without merge commits) + 1
+		revision=`git rev-list --no-merges --count HEAD`
+		echo $(($offset + $revision))
+		break
+	;;
+	'-c'|'--oscam-commit') 
+		sha=`git log 2>/dev/null | sed -n 1p | cut -d ' ' -f2 | cut -c1-8`
+		echo $sha
 		break
 	;;
 	'-O'|'--detect-osx-sdk-version')

globals.h
@@ -368,9 +368,12 @@
 /* ===========================
  *			constants
  * =========================== */
-#define CS_VERSION				"1.30_svn"
+#define CS_VERSION				"1.30"
 #ifndef CS_SVN_VERSION
 # define CS_SVN_VERSION			"test"
+#endif
+#ifndef CS_GIT_COMMIT
+# define CS_GIT_COMMIT			"a2b4c8d8"
 #endif
 #ifdef CS_CACHEEX
 #ifdef CS_CACHEEX_AIO

module-cccam.c
@@ -2820,11 +2820,11 @@ int32_t cc_parse_msg(struct s_client *cl, uint8_t *buf, int32_t l)
 				{
 					uint8_t token[256];
 #ifdef CS_CACHEEX_AIO
-					snprintf((char *)token, sizeof(token), "PARTNER: OSCam v%s, build r%s (%s) [EXT,SID,SLP,LGF]",
+					snprintf((char *)token, sizeof(token), "PARTNER: OSCam v%s, build r%s@%s (%s) [EXT,SID,SLP,LGF]",
 #else
-					snprintf((char *)token, sizeof(token), "PARTNER: OSCam v%s, build r%s (%s) [EXT,SID,SLP]",
+					snprintf((char *)token, sizeof(token), "PARTNER: OSCam v%s, build r%s@%s (%s) [EXT,SID,SLP]",
 #endif
-								CS_VERSION, CS_SVN_VERSION, CS_TARGET);
+								CS_VERSION, CS_SVN_VERSION, CS_GIT_COMMIT, CS_TARGET);
 
 					cc_cmd_send(cl, token, cs_strlen((char *)token) + 1, MSG_CW_NOK1);
 				}
@@ -3164,8 +3164,8 @@ int32_t cc_parse_msg(struct s_client *cl, uint8_t *buf, int32_t l)
 						}
 
 						uint8_t token[256];
-						snprintf((char *)token, sizeof(token), "PARTNER: OSCam v%s, build r%s (%s)%s",
-								CS_VERSION, CS_SVN_VERSION, CS_TARGET, param);
+						snprintf((char *)token, sizeof(token), "PARTNER: OSCam v%s, build r%s@%s (%s)%s",
+								CS_VERSION, CS_SVN_VERSION, CS_GIT_COMMIT, CS_TARGET, param);
 
 						cc_cmd_send(cl, token, cs_strlen((char *)token) + 1, MSG_CW_NOK1);
 					}

module-dvbapi.c
@@ -673,8 +673,8 @@ int32_t dvbapi_net_send(uint32_t request, int32_t socket_fd, uint32_t msgid, int
 				cs_strncat(capabilities, ",e2", sizeof(capabilities)); // usage of DES algo signalled through PID index - CSA and DES only
 			}
 
-			*info_len = snprintf((char *) &packet[size], sizeof(packet) - size, "OSCam v%s, build r%s (%s); %s",
-						CS_VERSION, CS_SVN_VERSION, CS_TARGET, capabilities + 1);
+			*info_len = snprintf((char *) &packet[size], sizeof(packet) - size, "OSCam v%s, build r%s@%s (%s); %s",
+						CS_VERSION, CS_SVN_VERSION, CS_GIT_COMMIT, CS_TARGET, capabilities + 1);
 
 			size += *info_len;
 			break;

module-lcd.c
@@ -87,6 +87,7 @@ static void refresh_lcd_file(void)
 
 			fprintf(fpsave, "Version: %s\n", CS_VERSION);
 			fprintf(fpsave, "Revision: %s\n", CS_SVN_VERSION);
+			fprintf(fpsave, "Commit: %s\n", CS_GIT_COMMIT);
 			if(days == 0)
 				{ fprintf(fpsave, "up: %02d:%02d:%02d\n", hours, mins, secs); }
 			else

module-monitor.c
@@ -364,7 +364,7 @@ static void monitor_send_details(char *txt, uint32_t tid)
 static void monitor_send_details_version(void)
 {
 	char buf[256];
-	snprintf(buf, sizeof(buf), "[V-0000]version=%s, build=%s, system=%s\n", CS_VERSION, CS_SVN_VERSION, CS_TARGET);
+	snprintf(buf, sizeof(buf), "[V-0000]version=%s, build=%s, commit=%s, system=%s\n", CS_VERSION, CS_SVN_VERSION, CS_GIT_COMMIT, CS_TARGET);
 	monitor_send_info(buf, 1);
 }
 
@@ -377,7 +377,7 @@ static void monitor_send_keepalive_ack(void)
 
 static void monitor_process_details_master(char *buf, uint32_t pid)
 {
-	snprintf(buf, 256, "Version=%sr%s", CS_VERSION, CS_SVN_VERSION);
+	snprintf(buf, 256, "Version=%sr%s@%s", CS_VERSION, CS_SVN_VERSION, CS_GIT_COMMIT);
 	monitor_send_details(buf, pid);
 	snprintf(buf, 256, "System=%s", CS_TARGET);
 	monitor_send_details(buf, pid);

commit 490222a071a30702575845aefdc9a2932a88b357

Introduce new oscam versioning scheme
- based on calendar versioning
- fix cccam packet is too big regression (introduced in https://git.streamboard.tv/common/oscam/-/commit/8e8dc4c8a2cd1ac7a71ca6a7b46a939acd7cce63)
- update pipeline regarding versioning
- define URLs for SCM, WIKI and BOARD in one place
- rename README to README.md

.gitlab/oscam-ci.yml
@@ -33,8 +33,8 @@ setup:
   variables:
     GIT_STRATEGY: none
   rules:
-    - if: $CI_PIPELINE_SOURCE == 'merge_request_event'
-    - if: $CI_PIPELINE_SOURCE == 'schedule'
+    - if: $CI_PIPELINE_SOURCE == 'merge_request_event' && $SETUP_BUILD_ENV == 'True'
+    - if: $CI_PIPELINE_SOURCE == 'schedule' && $SETUP_BUILD_ENV == 'True'
   script:
     - !reference [.helper, runner-setup]
   allow_failure: true

.gitlab/oscam-helper.yml
@@ -184,7 +184,7 @@
 
     - |
       echo "Updating VERSION information in globals.h..."
-      sed -i "/# define CS_SVN_VERSION/s/.*/# define CS_SVN_VERSION\t\t\t\"$new_tag\"/" globals.h
+      sed -i "/^#define CS_VERSION/s/\.[0-9].*\"/.$new_ver\"/" globals.h
       git diff globals.h
       git add globals.h
       git commit --amend --no-edit

CMakeLists.txt
@@ -681,8 +681,6 @@ add_custom_target (
 	touch ${CMAKE_CURRENT_SOURCE_DIR}/oscam-log.c
 )
 
-execute_process (COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/config.sh --oscam-revision COMMAND tr -d '\n' OUTPUT_VARIABLE CS_SVN_VERSION)
-add_definitions ("-D'CS_SVN_VERSION=\"${CS_SVN_VERSION}\"'")
 execute_process (COMMAND ${CMAKE_CURRENT_SOURCE_DIR}/config.sh --oscam-commit COMMAND tr -d '\n' OUTPUT_VARIABLE CS_GIT_COMMIT)
 add_definitions ("-D'CS_GIT_COMMIT=\"${CS_GIT_COMMIT}\"'")
 
@@ -851,7 +849,7 @@ include (CPack)
 message (STATUS "")
 message (STATUS "  operating system: ${OSCamOperatingSystem}")
 message (STATUS "  target system: ${CS_TARGET}")
-message (STATUS "  revision: ${CS_SVN_VERSION}")
+message (STATUS "  revision: ${CS_VERSION}")
 message (STATUS "  commit: ${CS_GIT_COMMIT}")
 if (HAVE_LIBCRYPTO)
 	message (STATUS "  use system libcrypto functions")

Makefile
@@ -5,7 +5,6 @@ SHELL = /bin/sh
 .PHONY: all tests help README.build README.config simple default debug config menuconfig allyesconfig allnoconfig defconfig clean distclean
 
 VER     := $(shell ./config.sh --oscam-version)
-SVN_REV := $(shell ./config.sh --oscam-revision)
 GIT_SHA := $(shell ./config.sh --oscam-commit)
 
 uname_S := $(shell sh -c 'uname -s 2>/dev/null || echo not')
@@ -61,7 +60,6 @@ ifeq "$(shell ./config.sh --enabled MODULE_STREAMRELAY)" "Y"
 endif
 
 override STD_LIBS := -lm $(LIB_PTHREAD) $(LIB_DL) $(LIB_RT)
-override STD_DEFS := -D'CS_SVN_VERSION="$(SVN_REV)"'
 override STD_DEFS += -D'CS_GIT_COMMIT="$(GIT_SHA)"'
 override STD_DEFS += -D'CS_CONFDIR="$(CONF_DIR)"'
 
@@ -235,9 +233,9 @@ OBJDIR := $(BUILD_DIR)/$(TARGET)
 # These variables will be used to select only needed files for compilation
 -include $(OBJDIR)/config.mak
 
-OSCAM_BIN := $(BINDIR)/oscam-$(VER)r$(SVN_REV)@$(GIT_SHA)-$(subst cygwin,cygwin.exe,$(TARGET))
+OSCAM_BIN := $(BINDIR)/oscam-$(VER)@$(GIT_SHA)-$(subst cygwin,cygwin.exe,$(TARGET))
 TESTS_BIN := tests.bin
-LIST_SMARGO_BIN := $(BINDIR)/list_smargo-$(VER)r$(SVN_REV)@$(GIT_SHA)-$(subst cygwin,cygwin.exe,$(TARGET))
+LIST_SMARGO_BIN := $(BINDIR)/list_smargo-$(VER)@$(GIT_SHA)-$(subst cygwin,cygwin.exe,$(TARGET))
 
 # Build list_smargo-.... only when WITH_LIBUSB build is requested.
 ifndef USE_LIBUSB
@@ -403,7 +401,7 @@ all:
 	@-mkdir -p $(OBJDIR)/cscrypt $(OBJDIR)/csctapi $(OBJDIR)/minilzo $(OBJDIR)/webif
 	@-printf "\
 +-------------------------------------------------------------------------------\n\
-| OSCam ver: $(VER) rev: $(SVN_REV) sha: $(GIT_SHA) target: $(TARGET)\n\
+| OSCam ver: $(VER) sha: $(GIT_SHA) target: $(TARGET)\n\
 | Tools:\n\
 |  CROSS    = $(CROSS_DIR)$(CROSS)\n\
 |  CC       = $(CC)\n\
@@ -713,8 +711,8 @@ OSCam build system documentation\n\
 \n\
    OSCAM_BIN=text  - This variable controls how the oscam binary will be named.\n\
                      Default OSCAM_BIN value is:\n\
-                      'BINDIR/oscam-VERSVN_REV@$GIT_SHA-TARGET'\n\
-                     Once the variables (BINDIR, VER, SVN_REV, GIT_SHA and TARGET) are\n\
+                      'BINDIR/oscam-VER@$GIT_SHA-TARGET'\n\
+                     Once the variables (BINDIR, VER, GIT_SHA and TARGET) are\n\
                      replaced, the resulting filename can look like this:\n\
                       'Distribution/oscam-1.20-unstable_svn7404-i486-slackware-linux-static'\n\
                      For example you can run: 'make OSCAM_BIN=my-oscam'\n\

config.sh
@@ -97,7 +97,7 @@ Usage: `basename $0` [parameters]
  -R, --restore             Restore default config.
 
  -v, --oscam-version       Display OSCam version.
- -r, --oscam-revision      Display OSCam SVN revision.
+ -r, --oscam-revision      Display OSCam SVN revision. //DEPRECATED, will be removed in later versions
  -c, --oscam-commit        Display OSCam GIT short commit sha 8-digits.
 
  -O, --detect-osx-sdk-version  Find where OS X SDK is located
@@ -700,20 +700,12 @@ do
 		break
 	;;
 	'-v'|'--oscam-version')
-		grep CS_VERSION globals.h | cut -d\" -f2
+		version=`grep '^#define CS_VERSION' globals.h | cut -d\" -f2`
+		echo $version
 		break
 	;;
-	'-r'|'--oscam-revision')
-		#get revision based on latest git tag
-		revision=`git describe --tags --abbrev=0 2>/dev/null`
-		if [ -z $revision ]; then
-			#get revision based on git-svn-id in commit message
-			revision=`git log -10 --pretty=%B 2>/dev/null | grep git-svn-id | head -n 1 | sed -n -e 's/^.*trunk@\([0-9]*\) .*$/\1/p'`
-		fi
-		if [ -z $revision ]; then
-			#get revision based on globals.h (not a git repository)
-			revision=`grep '# define CS_SVN_VERSION' globals.h | cut -d\" -f2`
-		fi
+	'-r'|'--oscam-revision') #//DEPRECATED, will be removed in later versions
+		revision=`grep '^#define CS_VERSION' globals.h | awk -F'-|"' '{print $3}'`
 		echo $revision
 		break
 	;;

globals.h
@@ -368,16 +368,16 @@
 /* ===========================
  *			constants
  * =========================== */
-#define CS_VERSION				"1.30"
-#ifndef CS_SVN_VERSION
-# define CS_SVN_VERSION			"11772"
-#endif
+#define SCM_URL					"https://git.streamboard.tv/common/oscam"
+#define WIKI_URL				"https://wiki.streamboard.tv/wiki"
+#define BOARD_URL				"https://board.streamboard.tv"
+#define CS_VERSION				"2.24.04-11773"
 #ifndef CS_GIT_COMMIT
 # define CS_GIT_COMMIT			"a2b4c6d8"
 #endif
 #ifdef CS_CACHEEX
 #ifdef CS_CACHEEX_AIO
-#define CS_AIO_VERSION			CS_SVN_VERSION
+#define CS_AIO_VERSION			CS_VERSION
 #endif
 #endif
 #ifndef CS_TARGET

module-camd35-cacheex.c
@@ -666,11 +666,11 @@ void camd35_cacheex_feature_trigger(struct s_client *cl, int32_t feature, uint8_
 			break;
 		// aio-version
 		case 32: ;
-			size += 12;
+			size += 14;
 			if(size < 32)
 				size = 32;
 
-			uint8_t token[12];
+			uint8_t token[14];
 
 			// bitfield
 			i2b_buf(2, feature, payload + i);

module-cccam-cacheex.c
@@ -660,7 +660,7 @@ void cc_cacheex_feature_trigger(struct s_client *cl, int32_t feature, uint8_t mo
 			break;
 		// aio-version
 		case 32: ;
-			uint8_t token[12];
+			uint8_t token[14];
 
 			// bitfield
 			i2b_buf(2, feature, payload + i);

module-cccam.c
@@ -2820,11 +2820,11 @@ int32_t cc_parse_msg(struct s_client *cl, uint8_t *buf, int32_t l)
 				{
 					uint8_t token[256];
 #ifdef CS_CACHEEX_AIO
-					snprintf((char *)token, sizeof(token), "PARTNER: OSCam v%s, build r%s@%s (%s) [EXT,SID,SLP,LGF]",
+					snprintf((char *)token, sizeof(token), "PARTNER: OSCam %s (%s) [EXT,SID,SLP,LGF]",
 #else
-					snprintf((char *)token, sizeof(token), "PARTNER: OSCam v%s, build r%s@%s (%s) [EXT,SID,SLP]",
+					snprintf((char *)token, sizeof(token), "PARTNER: OSCam %s (%s) [EXT,SID,SLP]",
 #endif
-								CS_VERSION, CS_SVN_VERSION, CS_GIT_COMMIT, CS_TARGET);
+								CS_VERSION, CS_TARGET);
 
 					cc_cmd_send(cl, token, cs_strlen((char *)token) + 1, MSG_CW_NOK1);
 				}
@@ -3164,8 +3164,8 @@ int32_t cc_parse_msg(struct s_client *cl, uint8_t *buf, int32_t l)
 						}
 
 						uint8_t token[256];
-						snprintf((char *)token, sizeof(token), "PARTNER: OSCam v%s, build r%s@%s (%s)%s",
-								CS_VERSION, CS_SVN_VERSION, CS_GIT_COMMIT, CS_TARGET, param);
+						snprintf((char *)token, sizeof(token), "PARTNER: OSCam %s (%s)%s",
+								CS_VERSION, CS_TARGET, param);
 
 						cc_cmd_send(cl, token, cs_strlen((char *)token) + 1, MSG_CW_NOK1);
 					}

module-dvbapi.c
@@ -673,8 +673,8 @@ int32_t dvbapi_net_send(uint32_t request, int32_t socket_fd, uint32_t msgid, int
 				cs_strncat(capabilities, ",e2", sizeof(capabilities)); // usage of DES algo signalled through PID index - CSA and DES only
 			}
 
-			*info_len = snprintf((char *) &packet[size], sizeof(packet) - size, "OSCam v%s, build r%s@%s (%s); %s",
-						CS_VERSION, CS_SVN_VERSION, CS_GIT_COMMIT, CS_TARGET, capabilities + 1);
+			*info_len = snprintf((char *) &packet[size], sizeof(packet) - size, "OSCam %s (%s); %s",
+						CS_VERSION, CS_TARGET, capabilities + 1);
 
 			size += *info_len;
 			break;

module-lcd.c
@@ -86,8 +86,6 @@ static void refresh_lcd_file(void)
 			}
 
 			fprintf(fpsave, "Version: %s\n", CS_VERSION);
-			fprintf(fpsave, "Revision: %s\n", CS_SVN_VERSION);
-			fprintf(fpsave, "Commit: %s\n", CS_GIT_COMMIT);
 			if(days == 0)
 				{ fprintf(fpsave, "up: %02d:%02d:%02d\n", hours, mins, secs); }
 			else

module-monitor.c
@@ -364,7 +364,7 @@ static void monitor_send_details(char *txt, uint32_t tid)
 static void monitor_send_details_version(void)
 {
 	char buf[256];
-	snprintf(buf, sizeof(buf), "[V-0000]version=%s, build=%s, commit=%s, system=%s\n", CS_VERSION, CS_SVN_VERSION, CS_GIT_COMMIT, CS_TARGET);
+	snprintf(buf, sizeof(buf), "[V-0000]version=%s, system=%s\n", CS_VERSION, CS_TARGET);
 	monitor_send_info(buf, 1);
 }
 
@@ -377,7 +377,7 @@ static void monitor_send_keepalive_ack(void)
 
 static void monitor_process_details_master(char *buf, uint32_t pid)
 {
-	snprintf(buf, 256, "Version=%sr%s@%s", CS_VERSION, CS_SVN_VERSION, CS_GIT_COMMIT);
+	snprintf(buf, 256, "Version=%s", CS_VERSION);
 	monitor_send_details(buf, pid);
 	snprintf(buf, 256, "System=%s", CS_TARGET);
 	monitor_send_details(buf, pid);

module-webif-tpl.c
@@ -588,7 +588,7 @@ int32_t tpl_saveIncludedTpls(const char *path)
 		{
 			if(strncmp(tpl->tpl_name, "IC", 2) != 0)
 			{
-				fprintf(fp, "<!--OSCam;%d;%s;%s@%s;%s-->\n", crc32(0, (uint8_t *)tpl->tpl_data, tpl->tpl_data_len), CS_VERSION, CS_SVN_VERSION, CS_GIT_COMMIT, tpl->tpl_deps);
+				fprintf(fp, "<!--OSCam;%d;%s;%s-->\n", crc32(0, (uint8_t *)tpl->tpl_data, tpl->tpl_data_len), CS_VERSION, tpl->tpl_deps);
 			}
 			fwrite(tpl->tpl_data, tpl->tpl_data_len, 1, fp);
 			fclose(fp);
@@ -638,7 +638,7 @@ void tpl_checkOneDirDiskRevisions(const char *subdir)
 				else { error = 0; }
 			}
 			else { cs_log("WARNING: Your http disk template %s is in the old template format without revision info. Please consider upgrading it!", path); }
-			if(error) { cs_log("If you are sure that it is current, add the following line at the beginning of the template to suppress this warning: <!--OSCam;%lu;%s;%s@%s;%s-->", curchecksum, CS_VERSION, CS_SVN_VERSION, CS_GIT_COMMIT, ifdefs); }
+			if(error) { cs_log("If you are sure that it is current, add the following line at the beginning of the template to suppress this warning: <!--OSCam;%lu;%s;%s-->", curchecksum, CS_VERSION, ifdefs); }
 			NULLFREE(tplorg);
 		}
 	}

module-webif.c
@@ -9320,8 +9320,10 @@ static int32_t process_request(FILE * f, IN_ADDR_T in)
 
 			localtime_r(&t, &lt);
 
+			tpl_addVar(vars, TPLADD, "SCM_URL", SCM_URL);
+			tpl_addVar(vars, TPLADD, "WIKI_URL", WIKI_URL);
+			tpl_addVar(vars, TPLADD, "BOARD_URL", BOARD_URL);
 			tpl_addVar(vars, TPLADD, "CS_VERSION", CS_VERSION);
-			tpl_addVar(vars, TPLADD, "CS_SVN_VERSION", CS_SVN_VERSION);
 			tpl_addVar(vars, TPLADD, "CS_GIT_COMMIT", CS_GIT_COMMIT);
 			tpl_addVar(vars, TPLADD, "CS_TARGET", CS_TARGET);
 			tpl_addVar(vars, TPLADD, "HTTPOSCAMLABEL", xml_encode(vars,cfg.http_oscam_label));
@@ -9359,7 +9361,7 @@ static int32_t process_request(FILE * f, IN_ADDR_T in)
 				tpl_addVar(vars, TPLADD, "REFRESH", tpl_getTpl(vars, "REFRESH"));
 			}
 #ifdef WEBIF_JQUERY
-			tpl_printf(vars, TPLADD, "SRCJQUERY", "jquery.js?v=%s-%s", CS_SVN_VERSION, CS_GIT_COMMIT);
+			tpl_printf(vars, TPLADD, "SRCJQUERY", "jquery.js?v=%s", CS_VERSION);
 #else
 			tpl_addVar(vars, TPLADD, "SRCJQUERY", cfg.http_extern_jquery);
 #endif

oscam-conf.c
@@ -511,10 +511,10 @@ FILE *create_config_file(const char *conf_filename)
 		cs_log("ERROR: Cannot create file \"%s\" (errno=%d %s)", temp_file, errno, strerror(errno));
 		return NULL;
 	}
-	fprintf(f, "# %s generated automatically by Streamboard OSCAM %s SVN r%s@%s\n",
-			conf_filename, CS_VERSION, CS_SVN_VERSION, CS_GIT_COMMIT);
-	fprintf(f, "# Read more: https://git.streamboard.tv/common/oscam/-/blob/master/Distribution/doc/txt/%s.txt\n\n",
-			conf_filename);
+	fprintf(f, "# %s generated automatically by Streamboard OSCam %s\n",
+			conf_filename, CS_VERSION);
+	fprintf(f, "# Read more: %s/blob/master/Distribution/doc/txt/%s.txt\n\n",
+			SCM_URL, conf_filename);
 	return f;
 }
 

oscam-config.c
@@ -756,7 +756,7 @@ int32_t init_srvid(void)
 		if(nr > 2000)
 		{
 			cs_log("WARNING: You risk high CPU load and high ECM times with more than 2000 service-id's!");
-			cs_log("HINT: --> use optimized lists from https://wiki.streamboard.tv/wiki/Srvid");
+			cs_log("HINT: --> use optimized lists from %s/Srvid", WIKI_URL);
 		}
 	}
 

oscam-ecm.c
@@ -1729,7 +1729,7 @@ static void logCWtoFile(ECM_REQUEST *er, uint8_t *cw)
 	if(writeheader)
 	{
 		/* no global macro for cardserver name :( */
-		fprintf(pfCWL, "# OSCam cardserver v%s - https://trac.streamboard.tv/oscam/\n", CS_VERSION);
+		fprintf(pfCWL, "# OSCam cardserver v%s - %s\n", CS_VERSION, SCM_URL);
 		fprintf(pfCWL, "# control word log file for use with tsdec offline decrypter\n");
 		strftime(buf, sizeof(buf), "DATE %Y-%m-%d, TIME %H:%M:%S, TZ %Z\n", &timeinfo);
 		fprintf(pfCWL, "# %s", buf);

oscam-log.c
@@ -238,7 +238,7 @@ int32_t cs_open_logfiles(void)
 	// according to syslog docu: calling closelog is not necessary and calling openlog multiple times is safe
 	// We use openlog to set the default syslog settings so that it's possible to allow switching syslog on and off
 	openlog(syslog_ident, LOG_NDELAY | LOG_PID, LOG_DAEMON);
-	cs_log(">> OSCam << cardserver %s, version " CS_VERSION ", build r" CS_SVN_VERSION "@" CS_GIT_COMMIT " (" CS_TARGET ")", starttext);
+	cs_log(">> OSCam << cardserver %s, version " CS_VERSION "@" CS_GIT_COMMIT " (" CS_TARGET ")", starttext);
 
 	return (fp <= (FILE *)0);
 }

oscam.c
@@ -137,11 +137,11 @@ static void show_usage(void)
 		   "| | | \\___ \\| |  / _` | '_ ` _ \\\n"
 		   "| |_| |___) | |_| (_| | | | | | |\n"
 		   " \\___/|____/ \\___\\__,_|_| |_| |_|\n\n");
-	printf("OSCam Cardserver v%s, build r%s@%s (%s)\n", CS_VERSION, CS_SVN_VERSION, CS_GIT_COMMIT, CS_TARGET);
+	printf("OSCam Cardserver v%s@%s (%s)\n", CS_VERSION, CS_GIT_COMMIT, CS_TARGET);
 	printf("Copyright (C) 2009-2024 OSCam developers.\n");
 	printf("This program is distributed under GPLv3.\n");
 	printf("OSCam is based on Streamboard mp-cardserver v0.9d written by dukat\n");
-	printf("Visit https://board.streamboard.tv/ for more details.\n\n");
+	printf("Visit %s for more details.\n\n", BOARD_URL);
 
 	printf(" ConfigDir  : %s\n", CS_CONFDIR);
 	printf("\n");
@@ -362,7 +362,7 @@ static void write_versionfile(bool use_stdout)
 				st.tm_hour, st.tm_min, st.tm_sec);
 	}
 
-	fprintf(fp, "Version:        oscam-%s-r%s@%s\n", CS_VERSION, CS_SVN_VERSION, CS_GIT_COMMIT);
+	fprintf(fp, "Version:        %s@%s\n", CS_VERSION, CS_GIT_COMMIT);
 	fprintf(fp, "Compiler:       %s\n", CS_TARGET);
 	fprintf(fp, "Box type:       %s (%s)\n", boxtype_get(), boxname_get());
 	fprintf(fp, "PID:            %d\n", getppid());
@@ -636,7 +636,7 @@ static void cs_dumpstack(int32_t sig)
 
 	fprintf(stderr, "crashed with signal %d on %swriting oscam.crash\n", sig, buf);
 
-	fprintf(fp, "%sOSCam cardserver v%s, build r%s@%s (%s)\n", buf, CS_VERSION, CS_SVN_VERSION, CS_GIT_COMMIT, CS_TARGET);
+	fprintf(fp, "%sOSCam cardserver v%s@%s (%s)\n", buf, CS_VERSION, CS_GIT_COMMIT, CS_TARGET);
 	fprintf(fp, "FATAL: Signal %d: %s Fault. Logged StackTrace:\n\n", sig, (sig == SIGSEGV) ? "Segmentation" : ((sig == SIGBUS) ? "Bus" : "Unknown"));
 	fclose(fp);
 

webif/api.xml/header.xml
@@ -1,2 +1,2 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<oscam version="##CS_VERSION## build r##CS_SVN_VERSION##@##CS_GIT_COMMIT##" revision="##CS_SVN_VERSION##" commit="##CS_GIT_COMMIT##" starttime="##APISTARTTIME##" uptime="##APIRUNTIME##" readonly="##APIREADONLY##">
+<oscam version="##CS_VERSION##" revision="" commit="##CS_GIT_COMMIT##" starttime="##APISTARTTIME##" uptime="##APIRUNTIME##" readonly="##APIREADONLY##">

webif/graph/graph.svg
@@ -7,7 +7,7 @@
 	preserveAspectRatio='XMaxYMid'
 	onload='init(evt)'
 	class='graph' >
- <title>##HTTPOSCAMLABEL## r##CS_SVN_VERSION##@##CS_GIT_COMMIT## (ECM Graph)</title>
+ <title>##HTTPOSCAMLABEL## ##CS_VERSION## (ECM Graph)</title>
  <g id='graph' >
 	<rect id='g' class='graph_bg' x='0' y='0' width='800' height='400' />
 	<text id='graph_error' class='graph_error' x='400' y='125' visibility='hidden'>Error occured!</text>

webif/include/footer.html
@@ -5,7 +5,7 @@
 			<LI CLASS="developed"><B>OSCam Webinterface developed by Streamboard Team</B></LI>
 			<LI><B>Current Time:</B><span id="curtime"> ##CURDATE## | ##CURTIME## </span><B>Start Time:</B> ##STARTDATE## | ##STARTTIME## <B>OSCam Run Time:</B><span id="runtime"> ##RUNTIME##</span></LI>
 			<LI><B>Process ID:</B> ##PROCESSID## <B>Access from:</B> ##CURIP## <B>Run as:</B> ##RUNAS## <B>##UPTIMETXT##</B><span id="uptime"> ##UPTIME##</span></LI>
-			<LI><B>OSCam:</B> ##CS_VERSION## <B>Build:</B> r##CS_SVN_VERSION##@##CS_GIT_COMMIT## <B>Compiler:</B> ##CS_TARGET##</LI>
+			<LI><B>Version:</B> ##CS_VERSION## <B>Git:</B> ##CS_GIT_COMMIT## <B>Compiler:</B> ##CS_TARGET##</LI>
 			<LI CLASS="styleauthor">WebIf Style by <A NAME="statusfooter"></A></LI>
 		</UL>
 	</DIV>

webif/include/header_short.html
@@ -2,12 +2,12 @@
    "http://www.w3.org/TR/html4/loose.dtd">
 <HTML>
 <HEAD>
-	<title>##HTTPOSCAMLABEL## r##CS_SVN_VERSION##@##CS_GIT_COMMIT##</title>
+	<title>##HTTPOSCAMLABEL## ##CS_VERSION##</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=##HTTP_CHARSET##">
 	<meta name="robots" content="noindex,nofollow">
 	<meta http-equiv="language" content="##LANGUAGE##">
-	<link rel="stylesheet" type="text/css" href="site.css?v=##CS_SVN_VERSION##-##CS_GIT_COMMIT##">
+	<link rel="stylesheet" type="text/css" href="site.css?v=##CS_VERSION##">
 	<link href="favicon.ico" rel="icon" type="image/x-icon">
 	<script type="text/javascript" src="##SRCJQUERY##"></script>
-	<script type="text/javascript" src="oscam.js?v=##CS_SVN_VERSION##-##CS_GIT_COMMIT##"></script>
+	<script type="text/javascript" src="oscam.js?v=##CS_VERSION##"></script>
 	<script type="text/javascript">var locale_decpoint = '##LOCALE_DECPOINT##';</script>

webif/include/logobit.html
@@ -1,1 +1,1 @@
-<DIV CLASS="logo"><A HREF="https://board.streamboard.tv/" TARGET="_blank" TITLE="OSCam Streamboard">##LOGO_INS##</A></DIV>
+<DIV CLASS="logo"><A HREF="##BOARD_URL##" TARGET="_blank" TITLE="OSCam Streamboard">##LOGO_INS##</A></DIV>

webif/status/status.html
@@ -19,9 +19,9 @@
 </div>
 <DIV ID="subnav">
 	<UL ID="nav">
-		<LI><B>##HTTPOSCAMLABEL## r##CS_SVN_VERSION##@##CS_GIT_COMMIT##</B></LI>
-		<LI CLASS="configmenu"><A HREF="https://git.streamboard.tv/common/oscam/-/commit/##CS_GIT_COMMIT##" TARGET="_blank">Commit</A></LI>
-		<LI CLASS="configmenu"><A HREF="https://git.streamboard.tv/common/oscam/-/commits/master" TARGET="_blank">Timeline</A></LI>
+		<LI><B>##HTTPOSCAMLABEL## ##CS_VERSION##</B></LI>
+		<LI CLASS="configmenu"><A HREF="##SCM_URL##/commit/##CS_GIT_COMMIT##" TARGET="_blank">Commit</A></LI>
+		<LI CLASS="configmenu"><A HREF="##SCM_URL##/commits/master" TARGET="_blank">Timeline</A></LI>
 		<LI CLASS="configmenu"><A HREF="#statusfooter">Status</A></LI>
 ##TPLPOLLINGSET##
 	</UL>