88d9daaa09f6b4a8069a62b37c13ea207278fdc2
WXbet <4-WXbet@users.noreply.git.streamboard.tv>
2024-09-16 14:00:06 +0000

    Replace readdir with scandir
    * Replace readdir with scandir
    
    - drop readdir: it is easier to use scandir, which also webif code uses
    - scandir should not need lock, as it is threadsafe
    
    Co-authored-by: @lpm11

--- module-webif.c
+++ module-webif.c
@@ -1469,8 +1469,7 @@ static char *send_oscam_config_webif(struct templatevars *vars, struct uriparams
 	}
 
 	struct dirent **namelist;
-	int count;
-	count = scandir(cs_confdir, &namelist, 0, alphasort );
+	int count = scandir(cs_confdir, &namelist, 0, NULL);
 
 	if( count >= 0 )
 	{
