d8ea52c52ea5770313fdd61ab1866aefafdd8388
ohmza <3435-ohmza@users.noreply.git.streamboard.tv>
2024-10-09 01:44:34 +0000

    Re-added some crypto functions as they are still useful (part 5 of 6)

--- cscrypt/sha256.c
+++ cscrypt/sha256.c
@@ -296,3 +296,17 @@ void mbedtls_sha256_finish( mbedtls_sha256_context *ctx, unsigned char output[32
 	if( ctx->is224 == 0 )
 		PUT_UINT32_BE( ctx->state[7], output, 28 );
 }
+
+/*
+ * output = SHA-256( input buffer )
+ */
+void mbedtls_sha256( const unsigned char *input, size_t ilen, unsigned char output[32], int is224 )
+{
+	mbedtls_sha256_context ctx;
+
+	mbedtls_sha256_init( &ctx );
+	mbedtls_sha256_starts( &ctx, is224 );
+	mbedtls_sha256_update( &ctx, input, ilen );
+	mbedtls_sha256_finish( &ctx, output );
+	mbedtls_sha256_free( &ctx );
+}
